02f455360b60a6868fcf76fbded26e632e742280
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-09-19 23:42:06 +0300

    [text] Introduce tfb_draw_string()

--- src/text.c
+++ src/text.c
@@ -14,12 +14,11 @@ void tfb_draw_char(u32 x, u32 y, u32 color, u8 c)
    psf2_header *h = current_font;
 
    if (!h) {
-      fprintf(stderr, "[tfblib] Error: no font currently selected\n");
-      abort();
+      fprintf(stderr, "[tfblib] ERROR: no font currently selected\n");
+      return;
    }
 
-   // ASSUMPTION: width is divisible by 8
-   const u32 width_bytes = h->width >> 3;
+   const u32 width_bytes = h->bytes_per_glyph / h->height;
 
    u8 *data = (u8 *)h + h->header_size + h->bytes_per_glyph * c;
 
@@ -28,4 +27,20 @@ void tfb_draw_char(u32 x, u32 y, u32 color, u8 c)
          for (u32 bit = 0; bit < 8; bit++)
             if ((data[b + width_bytes * row] & (1 << bit)))
                tfb_draw_pixel(x + (b << 3) + 8 - bit - 1, y + row, color);
+}
+
+void tfb_draw_string(u32 x, u32 y, u32 color, const char *s)
+{
+   psf2_header *h = current_font;
+
+   if (!h) {
+      fprintf(stderr, "[tfblib] ERROR: no font currently selected\n");
+      return;
+   }
+
+   while (*s) {
+      tfb_draw_char(x, y, color, *s);
+      x += h->width;
+      s++;
+   }
 }
